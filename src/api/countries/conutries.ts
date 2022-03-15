0e2d98773f83050d0923fdf1b4dd1b67cc9a367a
YongJin-Jo <artinc5233@gmail.com>
2022-03-15 23:14:02 +0900

    api 추가, api 타입 정의

--- src/api/countries/conutries.ts
+++ src/api/countries/conutries.ts
@@ -1,9 +1,32 @@
 import { AxiosResponse } from 'axios';
 import { conturey } from '../index';
 
+//전세계 통계
+function fetchSummary<T>(): Promise<AxiosResponse<T>> {
+  return conturey.get('summary');
+}
+
 // 국가 리스트 생성
 function fetchContury<T>(): Promise<AxiosResponse<T>> {
   return conturey.get('countries');
 }
 
-export { fetchContury };
+// 국가 라이브 상태
+function LiveByCountryAllStatus<T>(
+  country: string | null
+): Promise<AxiosResponse<T>> {
+  return conturey.get(`live/country/${country}`);
+}
+
+// 국가 전체 상태
+function ByCountryTotalAllStatus<T>(
+  country: string | null
+): Promise<AxiosResponse<T>> {
+  return conturey.get(`country/${country}`);
+}
+export {
+  fetchSummary,
+  fetchContury,
+  ByCountryTotalAllStatus,
+  LiveByCountryAllStatus,
+};
--- src/type/country.ts
+++ src/type/country.ts
@@ -1,5 +1,35 @@
-export interface CountryDefine {
+type CountryStatus = 'confirmed' | 'recovered' | 'deaths';
+
+interface CountryListDefine {
   Country: string;
   Slug: string;
   ISO2: string;
 }
+
+interface CountryDefault {
+  Country: string;
+  CountryCode: string;
+  Lat: string;
+  Lon: string;
+}
+
+interface CountryDefine extends CountryDefault {
+  Cases: number;
+  Status: string;
+  Date: string;
+}
+
+interface LiveCountryStatusDefine extends CountryDefault {
+  Confirmed?: number;
+  Deaths?: number;
+  Recovered?: number;
+  Active?: number;
+  LocationID: string;
+}
+
+export type {
+  CountryListDefine,
+  CountryDefine,
+  CountryStatus,
+  LiveCountryStatusDefine,
+};
