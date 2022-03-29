c204c18b01070d57521b22a98b34eddeeb029aac
artinc5233 <artinc5233@gmail.com>
2022-03-29 16:45:14 +0900

    차트 데이터 라벨 비활성화 , css 작성

--- src/components/moduler/chartBoard/ChartBoard.tsx
+++ src/components/moduler/chartBoard/ChartBoard.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ApexChart from 'react-apexcharts';
+import styled from 'styled-components';
 
 interface IProps {
   chart:
@@ -11,21 +12,22 @@ interface IProps {
 }
 
 export const ChartBoard = ({ chart }: IProps) => {
-  console.log(chart);
-
   return (
-    <div>
-      {chart && (
+    <Wrapper>
+      {chart === null ? (
+        <Contents>
+          <h1>World Covid 19 Summary</h1>
+        </Contents>
+      ) : (
         <ApexChart
           type="area"
+          height={'95%'}
           options={{
+            dataLabels: { enabled: false },
             chart: {
-              height: 100,
-              width: 100 + '%',
               toolbar: {
                 show: false,
               },
-              background: 'transparent',
             },
             yaxis: { show: false },
             xaxis: {
@@ -33,10 +35,34 @@ export const ChartBoard = ({ chart }: IProps) => {
               axisTicks: { show: false },
               labels: { show: false },
             },
+            legend: {
+              labels: {
+                colors: 'white',
+              },
+            },
           }}
           series={chart?.map(item => item)}
         />
       )}
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+
+const Contents = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: white;
+    font-size: 5rem;
+  }
+`;
